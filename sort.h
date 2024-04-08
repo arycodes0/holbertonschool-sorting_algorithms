55ecc398f0e16a2c930e895563a3ec0607d8fd73
Thaigerhoo <jhhonathhan42@gmail.com>
2024-04-08 01:50:08 -0400

    header files

---
+++ sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_array(const int *array, size_t size)
+void print_list(const listint_t *list)
+typedef struct listint_s
