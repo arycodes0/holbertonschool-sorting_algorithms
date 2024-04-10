cf8d61c0c5f8770f09873bbb074654c850b1bcd6
Ary <alvarezaryam@gmail.com>
2024-04-09 20:29:05 -0400

    Add line on header file

--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -29,3 +28,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+#endif
