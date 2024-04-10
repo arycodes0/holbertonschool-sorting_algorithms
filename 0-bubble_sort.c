4c797cd034e6ceb4fc3617cd2fee2b566fe633a0
Ary <alvarezaryam@gmail.com>
2024-04-09 20:35:12 -0400

    Fixed betty and if statement

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Func that sorts an array of integers in ascending order using Bubble sort
+ * bubble_sort - Func that sorts an array of integers in
+ * ascending order using Bubble sort
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
@@ -15,15 +16,15 @@ void bubble_sort(int *array, size_t size)
 		return;
 
 	/*Below we are sorting the array with the bubble method*/
-	for (i = 0; i < size -1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			/*Switching and ensuring that every element that are out of order are swapped,
-			 * bubbling the largest element to the end of the array*/
-			if (array[j] = array[j + 1])
+	/*Switching and ensuring that every element that are out of*/
+	/*order are swapped,bubbling the largest element to the end of the array*/
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				count++;
