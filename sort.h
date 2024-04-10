93ad41f8e09e00e0fb7b66383ab9cbd483ff5df8
Thaigerhoo <jhhonathhan42@gmail.com>
2024-04-10 16:05:48 -0400

    added prototypes to header file

--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void recursion(int *array, size_t size, int min, int max);
+int partition(int *array, size_t size, int min, int max);
+void swap(int *array, size_t size, int min, int max);
 #endif
